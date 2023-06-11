22391d2c9c93b02ae7e5d2201b475af102ee6d6a
phuriungung <puriung08@gmail.com>
2023-06-11 21:41:28 +0700

    Initial Commit

---
+++ multipleImagesAirdrop/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  multipleImagesAirdrop
+//
+//  Created by minto on 11/6/2566 BE.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
